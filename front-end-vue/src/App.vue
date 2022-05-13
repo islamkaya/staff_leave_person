328a63ecdca39df6a0fbdd5b62fe868a1b6fae22
islamkaya <islam16044@gmail.com>
2022-05-13 12:48:01 +0300

    inital commit front-end-vue

---
+++ front-end-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<LoginForm />
+</template>
+
+<script>
+import LoginForm from './views/LoginForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front-end-vue/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@/assets/css/bootstrap.css'
+import '@/assets/css/extra.css'
+
+
+createApp(App).mount('#app')
